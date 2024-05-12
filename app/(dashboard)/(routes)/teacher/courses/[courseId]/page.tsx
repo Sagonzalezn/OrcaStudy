fc6daebb6df3c9759317915abcb5b5e70275946b
Colmirage <semolinag@unal.edu.co>
2024-05-11 20:16:30 -0500

    hasta antes de Stripe

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -8,7 +8,7 @@ import { DescriptionForm } from "./_components/description-form";
 import ImageForm from "./_components/image-form";
 import { CategoryForm } from "./_components/category-form";
 import { PriceForm } from "./_components/price-form";
-import { AttachmentForm } from "./_components/attatchment-form";
+import { AttachmentForm } from "./_components/attachment-form";
 import { ChaptersForm } from "./_components/chapters-form";
 import { Banner } from "@/components/banner";
 import { Actions } from "./_components/actions";
