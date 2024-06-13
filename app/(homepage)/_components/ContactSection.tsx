8a44b16eb503a31012331d214404e1f39462d0d8
Sagonzalezn <sagonzalezn@unal.edu.co>
2024-06-13 12:12:01 -0500

    Cambio a jpg

--- app/(homepage)/_components/ContactSection.tsx
+++ app/(homepage)/_components/ContactSection.tsx
@@ -56,7 +56,7 @@ export const ContactSection = () => {
                     <div className='card'>
                         <div className='content'>
                             <div className='imgBx'>
-                                <img src='/samuel.png' alt='imagen'></img>
+                                <img src='/samuel.jpg' alt='imagen'></img>
                             </div>
                             <div className='contentBx'>
                                 <h4>Samuel Gonzalez</h4>
